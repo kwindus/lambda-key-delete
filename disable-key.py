8e538e4e68bcee9df7881d9c580b3e77721b40e5
Karen <kwindus@gmail.com>
2020-04-23 19:49:09 -0700

    lambda-python

--- disable-key.py
+++ disable-key.py
@@ -7,9 +7,10 @@ logger = logging.getLogger(__name__)
 logger.setLevel(logging.INFO)
 
 # ordered descending by days, take action on the first that matches
+# for safety I have not made calls to the API to actually disable or delete
 days_and_actions = (
-    (96, ['WARN_DELETED', 'DELETE']),
-    (90, ['WARN_DISABLED', 'DISABLE']),
+    (90, ['WARN_DELETED', 'DELETE']),
+    (89, ['WARN_DISABLED', 'DISABLE']),
     (70, ['WARN']),
 )
 
@@ -18,7 +19,7 @@ whitelisted_users = {'Admins'}
 whitelisted_groups = {'Admins'}
 
 def filtered_users(all_users, whitelisted=None):
- """
+    """
     Return filtered list of users, with whitelisted users excluded.
 
     >>> filtered_users([{'UserName': 'john'}, {'UserName': 'admin'}], whitelisted={'admin'})
@@ -32,13 +33,46 @@ def filtered_users(all_users, whitelisted=None):
     return [u for u in all_users if u['UserName'] not in whitelisted]
 
 
-def contains_whitelisted_group():
+def contains_whitelisted_group(user_groups, whitelisted=None):
+    """
+    Return True if any of the user groups is a whitelisted group.
+
+    >>> contains_whitelisted_group([{'GroupName': 'john'}, {'GroupName': 'admin'}], whitelisted={'admin'})
+    True
+
+    >>> contains_whitelisted_group([{'GroupName': 'john'}, {'GroupName': 'admin'}], whitelisted={})
+    False
+
+    """
     if whitelisted is None:
         whitelisted = whitelisted_groups
     return any(True for g in user_groups if g['GroupName'] in whitelisted)
 
 
-def decide_actions():
+def decide_actions(last_used, now):
+    """
+    Decide what to do with the access key, depending on the days elapsed since last access.
+
+    >>> decide_actions({}, datetime.now())
+    []
+
+    >>> decide_actions({'LastUsedDate': datetime.now()}, datetime.now())
+    []
+
+    >>> from datetime import timedelta
+    >>> decide_actions({'LastUsedDate': datetime.now() - timedelta(days=100)}, datetime.now())
+    ['WARN_DELETED', 'DELETE']
+
+    >>> decide_actions({'LastUsedDate': datetime.now() - timedelta(days=90)}, datetime.now())
+    ['WARN_DISABLED', 'DISABLE']
+
+    >>> decide_actions({'LastUsedDate': datetime.now() - timedelta(days=85)}, datetime.now())
+    ['WARN']
+
+    >>> decide_actions({'LastUsedDate': datetime.now() - timedelta(days=55)}, datetime.now())
+    []
+
+    """
     if 'LastUsedDate' in last_used:
         days_since_last_use = (now - last_used['LastUsedDate'].replace(tzinfo=None)).days
         for days_limit, actions in days_and_actions:
@@ -76,12 +110,29 @@ def take_action(action, access_key):
 
 
 def lambda_handler(event, context):
-    def lambda_handler(event, context):
-    #print(f"msg: hi")
     client = boto3.client('iam')
-    #print(f"msg:  {client}")
     now = datetime.utcnow()
 
+    all_users = client.list_users()['Users']
+    users = filtered_users(all_users)
+
+    for user in users:
+        username = user['UserName']
+        logger.info(username)
+
+        user_groups = client.list_groups_for_user(UserName=username)['Groups']
+        if contains_whitelisted_group(user_groups):
+            logger.info(whitelisted_groups)
+            continue
+
+        access_keys = client.list_access_keys(UserName=username)['AccessKeyMetadata']
+        for access_key in access_keys:
+            access_key_id = access_key['AccessKeyId']
+            print("msg: " + access_key_id)
+
+            last_used = client.get_access_key_last_used(AccessKeyId=access_key_id)['AccessKeyLastUsed']
+            for action in decide_actions(last_used, now):
+                take_action(action, access_key)
 
 if __name__ == '__main__':
     lambda_handler(None, None)
